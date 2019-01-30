29f58ce4a90a00e46d8fd1ab883aed63deb31dec
noble07 <juancmz98@gmail.com>
2019-01-29 22:45:47 -0500

    Productos:
    Se programa gestionar productos (Lista)

--- proyecto-php-poo/controllers/productoController.php
+++ proyecto-php-poo/controllers/productoController.php
@@ -1,10 +1,19 @@
 <?php
-
+require_once 'models/producto.php';
 class productoController{
 
     public function index(){
         // Renderizar vista
         require_once 'views/producto/destacados.php';
+    }
+
+    public function gestion(){
+        Utils::isAdmin();
+
+        $producto = new Producto();
+        $productos = $producto->getAll();
+
+        require_once 'views/producto/gestion.php';
     }
 }
 
--- proyecto-php-poo/views/layout/sidebar.php
+++ proyecto-php-poo/views/layout/sidebar.php
@@ -19,7 +19,7 @@
                     <ul>
                         <?php if(isset($_SESSION['admin'])): ?>
                         <li><a href="<?= base_url ?>categoria/index">Gestionar categorias</a></li>
-                        <li><a href="#">Gestionar productos</a></li>
+                        <li><a href="<?= base_url ?>producto/gestion">Gestionar productos</a></li>
                         <li><a href="#">Gestionar pedidos</a></li>
                         <?php endif; ?>
                         <?php if(isset($_SESSION['identity'])): ?>
