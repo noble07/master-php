3e12e3b229b952a1905595f122a7bfa1a8d4dcab
noble07 <juancmz98@gmail.com>
2019-02-07 00:04:25 -0500

    Carrito:
    Se programa vista del carrito y añadir al carrito en las demas pages

--- proyecto-php-poo/views/carrito/index.php
+++ proyecto-php-poo/views/carrito/index.php
@@ -9,7 +9,28 @@
                         $producto = $elemento['producto'];
                     ?>
                     <tr>
-                        <td><?= $producto->imagen ?></td>
+                        <td>
+                            <?php if($producto->imagen != null): ?>
+                            <img src="<?= base_url ?>uploads/images/<?= $producto->imagen ?>" class="img-carrito" alt="">
+                            <?php else: ?>
+                            <img src="<?= base_url ?>assets/img/camiseta.png" class="img-carrito" alt="">
+                            <?php endif; ?>
+                        </td>
+                        <td>
+                            <a href="<?= base_url ?>producto/ver&id=<?= $producto->id ?>"><?= $producto->nombre ?></a>
+                        </td>
+                        <td>
+                            <?= $producto->precio ?>
+                        </td>
+                        <td>
+                            <?= $elemento['unidades'] ?>
+                        </td>
                     </tr>
                     <?php endforeach; ?>
                 </table>
+                <br>
+                <div class="total-carrito">
+                    <?php $stats = Utils::statsCarrito(); ?>
+                    <h3>Precio total: <?= $stats['total']; ?> $</h3>
+                    <a href="" class="button button-pedido">Hacer pedido</a>
+                </div>
