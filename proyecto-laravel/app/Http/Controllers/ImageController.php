b64377e0949ff8d524316529b0e86d0419970644
noble07 <juancmz98@gmail.com>
2019-07-07 22:53:53 -0500

    Se programa el formulario del subir imagen

---
+++ proyecto-laravel/app/Http/Controllers/ImageController.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Facades\File;
+use App\Image;
+
+class ImageController extends Controller
+{
+    public function __construct(){
+        $this->middleware('auth');
+    }
+
+    public function create(){
+        return view('image.create');
+    }
+
+    public function save(Request $request){
+
+        //Validación
+        $validate = $this->validate($request, [
+            'description' => 'required',
+            'image_path' => 'required|image'
+        ]);
+
+        // Recoger los datos
+        $image_path = $request->file('image_path');
+        $description = $request->input('description');
+
+        // Asignar valores al objeto
+        $user = \Auth::user();
+        $image = new Image();
+        $image->user_id = $user->id;
+        $image->description = $description;
+        
+        // Subir fichero
+        if ($image_path) {
+            $image_path_name = time().$image_path->getClientOriginalName();
+            Storage::disk('images')->put($image_path_name, File::get($image_path));
+            $image->image_path = $image_path_name;
+        }
+
+        $image->save();
+
+        return redirect()->route('home')->with([
+            'message' => 'La foto ha sido subida correctamente!!'
+        ]);
+    }
+}
--- proyecto-laravel/routes/web.php
+++ proyecto-laravel/routes/web.php
@@ -46,3 +46,7 @@ Route::get('/configuracion', 'UserController@config')->name('config');
 Route::post('/user/update', 'UserController@update')->name('user.update');
 
 Route::get('/user/avatar/{filename}', 'UserController@getImage')->name('user.avatar');
+
+Route::get('/subir-imagen', 'ImageController@create')->name('image.create');
+
+Route::post('/image/save', 'ImageController@save')->name('image.save');
